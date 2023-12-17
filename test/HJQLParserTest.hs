fb11e649ca07339fa5d2f2a536812d9d7fa77923
zhaoer <zhaoer@seas.upenn.edu>
2023-12-17 14:57:34 -0500

    list query parsing tests

--- test/HJQLParserTest.hs
+++ test/HJQLParserTest.hs
@@ -12,136 +12,342 @@ runHJQLParser = P.parse hjqlP
 
 test_parseHJQLWPair :: Test
 test_parseHJQLWPair =
-  "parse write instructions"
-    ~: TestList
-      [ "empty"
-          ~: runHJQLParser
+  "parse write instructions" ~:
+    TestList
+      [ "empty" ~:
+          runHJQLParser
             "write {}"
-          ~?= Right (Write (QueryBranch [])),
-        "simple"
-          ~: runHJQLParser
+            ~?= Right (Write (QueryBranch [])),
+        "simple" ~:
+          runHJQLParser
             " write {\n\
             \    \"key\": \"value\",\n\
             \}\
             \"
-          ~?= Right (Write $ QueryBranch [QueryLeaf "key" (JSONStr "value")]),
-        "nested"
-          ~: runHJQLParser
+            ~?= Right (Write $ QueryBranch [QueryLeaf "key" (JSONStr "value")]),
+        "nested" ~:
+          runHJQLParser
             " write {\n\
             \    \"key\" {\
             \         \"otherkey\": 1\
             \      },\n\
             \}\n\
             \"
-          ~?= Right
-            ( Write $
-                QueryBranch
-                  [ QueryTwig "key" $
-                      QueryBranch [QueryLeaf "otherkey" $ JSONNum 1]
-                  ]
-            ),
-        "multiple, not nested"
-          ~: runHJQLParser
+            ~?= Right
+              ( Write $
+                  QueryBranch
+                    [ QueryTwig "key" $
+                        QueryBranch [QueryLeaf "otherkey" $ JSONNum 1]
+                    ]
+              ),
+        "multiple, not nested" ~:
+          runHJQLParser
             "write {\n\
             \    \"key\": null,\n\
             \    \"meow\": 0\n\
             \}\n\
             \"
-          ~?= Right
-            ( Write $
-                QueryBranch
-                  [ QueryLeaf "key" JSONNull,
-                    QueryLeaf "meow" $ JSONNum 0
-                  ]
-            ),
-        "multiple, nested, fancy JSON"
-          ~: runHJQLParser
+            ~?= Right
+              ( Write $
+                  QueryBranch
+                    [ QueryLeaf "key" JSONNull,
+                      QueryLeaf "meow" $ JSONNum 0
+                    ]
+              ),
+        "multiple, nested, fancy JSON" ~:
+          runHJQLParser
             "write {\n\
             \    \"key\": null,\n\
             \    \"meow\" {\n\
             \      \"warum\": {\"x\": \"hi\", \"y\": [\"bonk\", 0]}\n\
             \    }\n\
             \}"
-          ~?= Right
-            ( Write $
-                QueryBranch
-                  [ QueryLeaf "key" JSONNull,
-                    QueryTwig "meow" $
-                      QueryBranch
-                        [ QueryLeaf "warum" $
-                            JSONObj $
-                              M.fromList
-                                [ ("x", JSONStr "hi"),
-                                  ("y", JSONList [JSONStr "bonk", JSONNum 0])
+            ~?= Right
+              ( Write $
+                  QueryBranch
+                    [ QueryLeaf "key" JSONNull,
+                      QueryTwig "meow" $
+                        QueryBranch
+                          [ QueryLeaf "warum" $
+                              JSONObj $
+                                M.fromList
+                                  [ ("x", JSONStr "hi"),
+                                    ("y", JSONList [JSONStr "bonk", JSONNum 0])
+                                  ]
+                          ]
+                    ]
+              ),
+        "simple list" ~:
+          runHJQLParser
+            "write {\n\
+            \    \"key\" [\n\
+            \        \"field\": true\n\
+            \    ]\n\
+            \}"
+            ~?= Right
+              ( Write $
+                  QueryBranch
+                    [ QueryList
+                        "key"
+                        M.empty
+                        ( QueryBranch [QueryLeaf "field" $ JSONBool True]
+                        )
+                    ]
+              ),
+        "nested list/object" ~:
+          runHJQLParser
+            "write {\n\
+            \    \"key\" [\n\
+            \        \"field\": []\n,\
+            \        \"data\" [\n\
+            \            \"name\": \"Jacob\",\n\
+            \            \"boss\" {\n\
+            \                \"id\": 2\n\
+            \            }\n\
+            \        ]\n\
+            \    ]\n\
+            \}"
+            ~?= Right
+              ( Write $
+                  QueryBranch
+                    [ QueryList "key" M.empty $
+                        QueryBranch
+                          [ QueryLeaf "field" $ JSONList [],
+                            QueryList "data" M.empty $
+                              QueryBranch
+                                [ QueryLeaf "name" $ JSONStr "Jacob",
+                                  QueryTwig "boss" $
+                                    QueryBranch [QueryLeaf "id" $ JSONNum 2]
                                 ]
-                        ]
-                  ]
-            )
+                          ]
+                    ]
+              ),
+        "list filters" ~:
+          runHJQLParser
+            "write {\n\
+            \    \"key\" [\n\
+            \        \"field\": null\n,\
+            \        \"data\" |\
+            \ \"id\"  ==   300    && \"boss\" ==\
+            \{\"id\": 100, \"name\": \"Boris\"} [\n\
+            \            \"name\": \"Edward\",\n\
+            \            \"boss\" {\n\
+            \                \"id\": 552\n\
+            \            }\n\
+            \        ]\n\
+            \    ]\n\
+            \}"
+            ~?= Right
+              ( Write $
+                  QueryBranch
+                    [ QueryList "key" M.empty $
+                        QueryBranch
+                          [ QueryLeaf "field" JSONNull,
+                            QueryList
+                              "data"
+                              ( M.fromList
+                                  [ ("id", JSONNum 300),
+                                    ( "boss",
+                                      JSONObj $
+                                        M.fromList
+                                          [ ("id", JSONNum 100),
+                                            ("name", JSONStr "Boris")
+                                          ]
+                                    )
+                                  ]
+                              )
+                              ( QueryBranch
+                                  [ QueryLeaf "name" $ JSONStr "Edward",
+                                    QueryTwig "boss" $
+                                      QueryBranch
+                                        [ QueryLeaf "id" $
+                                            JSONNum 552
+                                        ]
+                                  ]
+                              )
+                          ]
+                    ]
+              )
       ]
+
+-- "write {\n\
+-- \    \"key\" [\n\
+-- \        \"field\": null,\n,\
+-- \        \"data\" |\
+-- \ \"id\"  ==   300    && \"boss\" ==\
+-- \{\"id\": 100, \"name\": \"Boris\"} [\n\
+-- \            \"name\": \"Edward\",\n\
+-- \            \"boss\" {\n\
+-- \                \"id\": 552\n\
+-- \            }\n\
+-- \        ]\n\
+-- \    ]\n\
+-- \}"
+-- >>> runHJQLParser "write {\n\
+-- \    \"key\" [\n\
+-- \        \"field\": null,\n,\
+-- \        \"data\" |\
+-- \ \"id\"  ==   300    && \"boss\" ==\
+-- \{\"id\": 100, \"name\": \"Boris\"} [\n\
+-- \            \"name\": \"Edward\",\n\
+-- \            \"boss\" {\n\
+-- \                \"id\": 552\n\
+-- \            }\n\
+-- \        ]\n\
+-- \    ]\n\
+-- \}"
 
 test_parseHJQLNoPair :: Test
 test_parseHJQLNoPair =
-  "parse read/delete instructions"
-    ~: TestList
-      [ "empty"
-          ~: runHJQLParser
+  "parse read/delete instructions" ~:
+    TestList
+      [ "empty" ~:
+          runHJQLParser
             "read {}"
-          ~?= Right (Read (QueryBranch [])),
-        "simple"
-          ~: runHJQLParser
+            ~?= Right (Read (QueryBranch [])),
+        "simple" ~:
+          runHJQLParser
             " delete {\n\
             \    \"key\",\n\
             \}\
             \"
-          ~?= Right (Delete $ QueryBranch [QueryLeaf "key" ()]),
-        "nested"
-          ~: runHJQLParser
+            ~?= Right (Delete $ QueryBranch [QueryLeaf "key" ()]),
+        "nested" ~:
+          runHJQLParser
             " delete {\n\
             \    \"key\" {\
             \         \"otherkey\"\
             \      },\n\
             \}\n\
             \"
-          ~?= Right
-            ( Delete $
-                QueryBranch
-                  [ QueryTwig "key" $
-                      QueryBranch [QueryLeaf "otherkey" ()]
-                  ]
-            ),
-        "multiple, not nested"
-          ~: runHJQLParser
+            ~?= Right
+              ( Delete $
+                  QueryBranch
+                    [ QueryTwig "key" $
+                        QueryBranch [QueryLeaf "otherkey" ()]
+                    ]
+              ),
+        "multiple, not nested" ~:
+          runHJQLParser
             "delete {\n\
             \    \"key\",\n\
             \    \"meow\"\n\
             \}\n\
             \"
-          ~?= Right
-            ( Delete $
-                QueryBranch
-                  [ QueryLeaf "key" (),
-                    QueryLeaf "meow" ()
-                  ]
-            ),
-        "multiple, nested"
-          ~: runHJQLParser
+            ~?= Right
+              ( Delete $
+                  QueryBranch
+                    [ QueryLeaf "key" (),
+                      QueryLeaf "meow" ()
+                    ]
+              ),
+        "multiple, nested" ~:
+          runHJQLParser
             "read {\n\
             \    \"key\",\n\
             \    \"meow\" {\n\
             \      \"warum\",\n\
             \    }\n\
             \}"
-          ~?= Right
-            ( Read $
-                QueryBranch
-                  [ QueryLeaf "key" (),
-                    QueryTwig "meow" $ QueryBranch [QueryLeaf "warum" ()]
-                  ]
-            )
+            ~?= Right
+              ( Read $
+                  QueryBranch
+                    [ QueryLeaf "key" (),
+                      QueryTwig "meow" $ QueryBranch [QueryLeaf "warum" ()]
+                    ]
+              ),
+        "simple list" ~:
+          runHJQLParser
+            "delete {\n\
+            \    \"key\" [\n\
+            \        \"field\"\n\
+            \    ]\n\
+            \}"
+            ~?= Right
+              ( Delete $
+                  QueryBranch
+                    [ QueryList
+                        "key"
+                        M.empty
+                        ( QueryBranch [QueryLeaf "field" ()]
+                        )
+                    ]
+              ),
+        "nested list/object" ~:
+          runHJQLParser
+            "read {\n\
+            \    \"key\" [\n\
+            \        \"field\"\n,\
+            \        \"data\" [\n\
+            \            \"name\",\n\
+            \            \"boss\" {\n\
+            \                \"id\"\n\
+            \            }\n\
+            \        ]\n\
+            \    ]\n\
+            \}"
+            ~?= Right
+              ( Read $
+                  QueryBranch
+                    [ QueryList "key" M.empty $
+                        QueryBranch
+                          [ QueryLeaf "field" (),
+                            QueryList "data" M.empty $
+                              QueryBranch
+                                [ QueryLeaf "name" (),
+                                  QueryTwig "boss" $
+                                    QueryBranch [QueryLeaf "id" ()]
+                                ]
+                          ]
+                    ]
+              ),
+        "list filters" ~:
+          runHJQLParser
+            "read {\n\
+            \    \"key\" [\n\
+            \        \"field\"\n,\
+            \        \"data\" |\
+            \ \"id\"  ==   300    && \"boss\" ==\
+            \{\"id\": 100, \"name\": \"Boris\"} [\n\
+            \            \"name\",\n\
+            \            \"boss\" {\n\
+            \                \"id\"\n\
+            \            }\n\
+            \        ]\n\
+            \    ]\n\
+            \}"
+            ~?= Right
+              ( Read $
+                  QueryBranch
+                    [ QueryList "key" M.empty $
+                        QueryBranch
+                          [ QueryLeaf "field" (),
+                            QueryList
+                              "data"
+                              ( M.fromList
+                                  [ ("id", JSONNum 300),
+                                    ( "boss",
+                                      JSONObj $
+                                        M.fromList
+                                          [ ("id", JSONNum 100),
+                                            ("name", JSONStr "Boris")
+                                          ]
+                                    )
+                                  ]
+                              )
+                              ( QueryBranch
+                                  [ QueryLeaf "name" (),
+                                    QueryTwig "boss" $
+                                      QueryBranch [QueryLeaf "id" ()]
+                                  ]
+                              )
+                          ]
+                    ]
+              )
       ]
 
 -- >>> runTestTT test_parseHJQLWPair
--- Counts {cases = 5, tried = 5, errors = 0, failures = 0}
+-- Counts {cases = 8, tried = 8, errors = 0, failures = 2}
 
 -- >>> runTestTT test_parseHJQLNoPair
--- Counts {cases = 5, tried = 5, errors = 0, failures = 0}
+-- Counts {cases = 8, tried = 8, errors = 0, failures = 0}
